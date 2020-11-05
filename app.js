f1cec6313fbcffee5ba00121bf23d7e4d42844f6
Cassandra Liau <cass@Cassandras-MacBook-Pro.local>
2020-11-06 04:39:10 +0800

    Update env value

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require("express");
 
 const app = express();
 
-const port = process.env.POST || 4000;
+const port = process.env.PORT || 4000;
 
 //routes import
 const indexRoute = require("./routes/index.js");
 
 //routes
-app.use(`/`, indexRoute);
+app.use("/", indexRoute);
 
 app.listen(port, () =>
   console.log(`Exercise four is running at localhost:${port}`)
